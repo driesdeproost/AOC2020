cd3428eb72168e8b3ed829e6bdc88babd2eedd3c
Dries De Proost <driesdeproost@gmail.com>
2020-12-02 22:44:39 +0100

    day 2

---
+++ 02/passwords-2.py
@@ -0,0 +1,38 @@
+from typing import List, Iterator
+
+
+class PasswordRestriction:
+  text: str = ""
+  min: int = 0
+  max: int = 0
+
+
+class PasswordCheck:
+    restriction: PasswordRestriction = None
+    password: str
+
+
+def parseLine(lst: List[str]) -> PasswordCheck:
+    check = PasswordCheck()
+    restr = PasswordRestriction()
+    minmax = lst[0].split("-")
+    restr.min = int(minmax[0])
+    restr.max = int(minmax[1])
+    restr.text = lst[1][:-1]
+    check.restriction = restr
+    check.password = lst[2]
+    return check
+
+
+def valid(check: PasswordCheck) -> bool:
+    restr: PasswordRestriction = check.restriction
+    return bool(check.password[restr.min-1] == restr.text) != (check.password[restr.max-1] == restr.text)
+
+lines: List[str]
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+
+splitBySpace: List[List[str]] = [x.split(" ") for x in lines]
+parsed: Iterator[PasswordCheck] = map(parseLine, splitBySpace)
+ok: Iterator[PasswordCheck] = filter(valid, parsed)
+print(len(list(ok)))
